e6cf3f7fa6218de2a0817f4644d5072706eb392f
Sheila Paloma <sheila.psb@gmail.com>
2023-07-16 17:07:55 -0300

    Update tela_exemplar.py

--- tela_exemplar.py
+++ tela_exemplar.py
@@ -12,6 +12,29 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Tela_Exemplar(object):
+    """
+    classe que representa a tela de cadastro de exemplar de livro, sendo esta especifica para administrador(es) do sistema.
+
+    ...
+    Atributes
+    ----------
+    resize : object
+        dimensiona largura e altura da tela
+    setStyleSheet : object
+        estiliza a cor de fundo da tela
+    setObjectName : object
+        rotula algum lugar da tela, onde se deseja definir um nome ou frase curta
+    setText :
+        O método setText() da classe java.text.CollationElementIterator é usado para definir a nova string de origem para o objeto
+    QLineEdit : object
+        É um campo ou caixa de texto.
+    QPushButton : object
+        atribuem-se aos botoes de acoes da tela
+    botao_cadastrar_exemplar :
+        elemento instanciado do QPushButton, que foi rotulado para identifica-lo como o botao para cadastrar um exemplar de livro.
+    botao_voltar_exemplar :
+        elemento instanciado do QPushButton, que foi rotulado para identifica-lo como o botao para voltar da tela de cadastro de exemplar para a tela principal da biblioteca para administrador(es) do sistema.
+    """
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1017, 749)
