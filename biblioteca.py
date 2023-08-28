9212c372d89d45f95c69bb321d98f6630f1ffc57
272188 <sheila.psb@gmail.com>
2023-08-27 22:07:57 -0300

    reestrutura do codigo

--- biblioteca.py
+++ biblioteca.py
@@ -1,40 +1,41 @@
 #importação das classes
-from pessoa import Pessoa
-from usuario import Usuario
-from autor import Autor
-from livro import Livro
-from exemplar import Exemplar
-from emprestimo import Emprestimo
-from devolucao import Devolucao
+from modelos import Usuario, Livro, Emprestimo, TABELAS
+
 
 from datetime import date
 
 #importação do banco de dados
-import mysql.connector 
+import mysql.connector
+
+usuario = 'sheila'
+senha = 'root'
 
 
 class Biblioteca:
     """
 
-	A classe principal, que faz a conexao com banco de dados e cria todas as tabelas, tambem foi utilizada para representar os principais metodos de funcionalidades do sistema
+	Classe que representa a biblioteca, responsável por realizar as operações no banco de dados.
 
     ...
 
     Attributes
     ----------
-    self.conexao : object
-        realiza a conexao entre a classe biblioteca  com o banco de dados, que requer os parametros de acesso e conexao
-    self.cursor : object
-        cursor mysql que permite executar comandos no banco de dados, será usado sempre que for necessário solicitar algo ao banco de dados 
+    usuario : Usuario
+        usuario que esta utilizando o sistema
+    conexao : mysql.connector.connect
+        conexao com o banco de dados
+    cursor : mysql.connector.cursor
+        cursor para realizar as operacoes no banco de dados
+ 
     
      Methods
     -------
-    verificarLogin(codigo_usuario, senha):
-        verfifica se o codigo do usuario e a senha corresponde a algum usuario cadastrado no sistema
-    cadastrarUsuario(usuario):
-        cadastra um usuario se o mesmo nao possuir cadastro
-    buscarUsuario(codigo_usuario):
-        faz uma busca entre os usuarios cadastrados na tabela de usuario, atraves do codigo de usuario informado
+    verificarLogin(self, email, senha):
+        verfifica se o email e a senha esta correto, usuario cadastrado no sistema
+    cadastrarUsuario(self, nome, email, senha, cpf, telefone, endereco, bairro, cidade, cep, is_admin=False):
+        cadastra um usuario no banco de dados.
+    buscarUsuario(self, email):
+        faz uma busca do usuario no bano de dados.
     cadastrarAutor(autor):
         cadastra um autor se o mesmo nao possuir cadastro no sistema
     buscarAutores(nome_autor):
@@ -57,7 +58,7 @@ class Biblioteca:
         faz uma busca entre entre as devolucoes realizadas na tabela de devolucao, atraves do codigo do exemplar e a data da devolucao informada
 	"""
    
-    __slots__ = ['conexao','cursor','mysql']
+    
     def __init__(self):
         """
         construtor que cria tres atributos referentes ao banco de dados
@@ -73,162 +74,174 @@ class Biblioteca:
             criar o banco de dados e suas respectivas tabelas caso os mesmos não exista
         """
         #configuração para conexão com o banco de dados
+        self.usuario = None
         self.conexao = mysql.connector.connect(
             host = 'localhost',
-            user = 'sheila',
-            password= 'root',
-            database='banco_bib',
+            user = usuario,
+            password= senha,
+            auth_plugin = 'mysql_native_password',
+        
         )
         self.cursor = self.conexao.cursor()
-        self.mysql = """CREATE DATABASE IF NOT EXISTS banco_bib"""
-        self.cursor.execute(self.mysql) #funcao para executar a acao no mysql
+        self.cursor.execute("CREATE DATABASE IF NOT EXISTS banco_bib") #funcao para executar a acao no mysql
+        self.cursor.execute("USE banco_bib")
+        for tabela in TABELAS:
+            self.cursor.execute(TABELAS[tabela])
         self.conexao.commit() #Por padrão, não é efetuado commit automaticamente, deve-se commitar para salvar as alteracoes no banco.
-
-        self.mysql = """USE banco_bib"""
-        self.cursor.execute(self.mysql)
-        self.conexao.commit()
         
-        self.mysql = """CREATE TABLE IF NOT EXISTS pessoa(nome VARCHAR(20) NOT NULL PRIMARY KEY, sobrenome VARCHAR(40) NOT NULL)"""
-
-        self.cursor.execute(self.mysql)
-        self.conexao.commit()
-
-        self.mysql = """CREATE TABLE IF NOT EXISTS usuario(codigo_usuario varchar(30) NOT NULL PRIMARY KEY, nome VARCHAR(100) NOT NULL, cpf VARCHAR(14) NOT NULL, telefone VARCHAR(20) NOT NULL, endereco VARCHAR(200) NOT NULL, bairro VARCHAR(100) NOT NULL, cidade VARCHAR(100) NOT NULL, cep VARCHAR(20) NOT NULL, email VARCHAR(100) NOT NULL, senha VARCHAR(30) NOT NULL, tipo VARCHAR(20) NOT NULL DEFAULT 'usuario')"""
-
-        self.cursor.execute(self.mysql)
-        self.conexao.commit()
-
-
-        self.mysql = """CREATE TABLE IF NOT EXISTS livro(codigo_livro varchar(30) NOT NULL PRIMARY KEY, nome_autor VARCHAR(45) NOT NULL, codigo_autor varchar(30) NOT NULL, titulo VARCHAR(200) NOT NULL, editora VARCHAR(45) NOT NULL, isbn VARCHAR(20) NOT NULL, assunto VARCHAR(100) NOT NULL, edicao varchar(30) NOT NULL, volume varchar(30) NOT NULL, Numero_pag varchar(30) NOT NULL, anoPublicacao varchar(30) NOT NULL, quantidade_exemplares int NOT NULL)"""
-
-        self.cursor.execute(self.mysql)
-        self.conexao.commit()
-
-        self.mysql = """CREATE TABLE IF NOT EXISTS exemplar(codigo_exemplar varchar(30) NOT NULL PRIMARY KEY, codigo_livro varchar(30) NOT NULL, dias_emprestimo varchar(30) NOT NULL)"""
-
-        self.cursor.execute(self.mysql)
-        self.conexao.commit()
-
-        self.mysql = """CREATE TABLE IF NOT EXISTS emprestimo(codigo_livro varchar(30) NOT NULL, codigo_exemplar varchar(30) NOT NULL, data_emprestimo VARCHAR(10) NOT NULL, data_para_devolver VARCHAR(10) NOT NULL)"""
-        self.cursor.execute(self.mysql)
-        self.conexao.commit()
-
-        self.mysql = """CREATE TABLE IF NOT EXISTS devolucao(codigo_usuario varchar(30) NOT NULL PRIMARY KEY, codigo_livro varchar(30) NOT NULL, codigo_exemplar varchar(30) NOT NULL, data_emprestimo VARCHAR(10) NOT NULL, data_devolucao VARCHAR(10) NOT NULL)"""
-        
-        self.cursor.execute(self.mysql)
-        self.conexao.commit()
         
 
-    def verificarLogin(self, codigo_usuario, senha):
+    def verificarLogin(self, email, senha):
         """
-        verifica se o codigo do usuario e senha informado corresponde aos dos usuarios ja cadastrados no sistema e assim, permitir logar no sistema.
+        verifica se o email e senha informado corresponde aos dos usuarios ja cadastrados no sistema e assim, permitir logar no sistema.
 
         Parameters
         ----------
-        codigo_usuario {string}:
-            codigo de identificao do usuario que podera conter numero e/ou caracteres especiais ao se cadastrar
+        email {string}:
+            email do usuario informado ao se cadastrar no sistema
         senha {string} :
             hash da senha em formato de string para garatir a seguranca no acesso ao sistema
         return {boolean}: 
             Boleano referente ao resultado para retorno das condicoes das funcoes
         """
 
-        selecionar = self.buscarUsuario(codigo_usuario) #selecionar recebe a funcao buscarUsuario, que busca o usuario por seu codigo cadastrado
-            
-        if selecionar == None:
-            return None
-        elif selecionar.senha != senha:
-            return False
-        else:
-            return selecionar
+        usuario = self.buscarUsuario(email)
+        if usuario and usuario.senha != senha:
+            usuario = None
+        self.usuario = usuario
+        return usuario
 
-    def cadastrarUsuario(self, usuario):
+    def cadastrarUsuario(self, nome, email, senha, cpf, telefone, endereco, bairro, cidade, cep, is_admin=False):
         """
-        verifica se o codigo do usuario informado corresponde aos dos usuarios ja cadastrados no sistema, caso nao, realiza um novo cadastro de usuario
+        verifica se oemail do usuario informado corresponde aos dos usuarios ja cadastrados no sistema, caso nao, solicita-se realizar o cadastro de novo usuario no sistema
 
         Parameters
         ----------
-        codigo_usuario {string}:
-            codigo de identificao do usuario em formato de string, que podera conter numero e/ou caracteres especiais ao se cadastrar
-        return {boolean}: 
-            referente ao retorno com o resultado em formato Boleano (verdadeiro ou falso) das condicoes das funcoes
+        nome {string}:
+            nome do usuario em formato de string.
+        email {string}:
+            email do usuario em formato de string.
+        senha {string}:
+            senha do usuario em formato de string, podera conter numeros e/ou caracteres especiais.
+        cpf {string}:
+            cpf do usuario em formato de string, podera conter caracteres especiais.
+        telefone {string}:
+            telefone do usuario em formato de string, podera conter caracteres especiais.
+        endereco {string}:
+            endereco de residencia em formato de string, podera ser o nome da rua/avenida/praca e o numero da residencia.
+        bairro {string}:
+            bairro de residencia em formato de string.
+        cidade {string}:
+            cidade de residencia em formato de string.
+        cep {string}:
+            cep de localizacao da residencia em formato de string.
+        is_admin {boolean}:
+            tipo de usuario para devido acesso as funcionalidades do sistema em formato de boolean.
         VALUES:
             refere-se formato de representacao dos valores das variaveis inseridas na tabela 
         """ 
-        verifica = self.buscarUsuario(usuario.codigo_usuario)
-        if verifica == None:
-            self.cursor.execute('INSERT INTO usuario(codigo_usuario, nome, cpf, telefone, endereco, bairro, cidade, cep, email, senha) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', (usuario.codigo_usuario, usuario.nome, usuario.cpf, usuario.telefone, usuario.endereco, usuario.bairro, usuario.cidade, usuario.cep, usuario.email, usuario.senha))
+        usuario = None
+        buscar = self.buscarUsuario(email)
+        if buscar == None:
+            self.cursor.execute('INSERT INTO usuario(nome, email, senha, cpf, telefone, endereco, bairro, cidade, cep, is_admin) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)', (nome, email, senha, cpf, telefone, endereco, bairro, cidade, cep, is_admin))
             self.conexao.commit()
-            return True
-        else:
-            return False
+            usuario = self.buscarUsuario(email)
+        return usuario
 
         
-    def buscarUsuario(self, codigo_usuario):
+    def buscarUsuario(self, email):
         """
         realiza a busca de um usuario pelo codigo do usuario informado.
 
         Parameters
         ----------
-        codigo_usuario {string}:
-            codigo de identificao do usuario em formato de string, que podera conter numero e/ou caracteres especiais ao se cadastrar
+        email {string}:
+            email do usuario em formato de string, informado ao se cadastrar.
         return {boolean}: 
             referente ao retorno com o resultado do tipo None ou o proprio resultado das condicoes das funcoes
         usuario:
-            referece um elemento intanciado da classe Usuario, que recebe uma lista. 
+            referece um elemento intanciado da classe Usuario, que recebe None como parametro padrao 
         """
-        self.cursor.execute("SELECT * FROM usuario WHERE codigo_usuario = %s", (codigo_usuario,)) #executa a selecao do codigo do usuario na tabela usuario
-        selecionar = self.cursor.fetchone() #recupera a próxima linha de um conjunto de resultados de consulta e retorna uma única sequência ou None, se não houver mais linhas disponíveis.
-        if (selecionar == None):
-            return None
-        else:
-            usuario = Usuario(selecionar[0],selecionar[1],selecionar[2],selecionar[3],selecionar[4],selecionar[5],selecionar[6],selecionar[7],selecionar[8],selecionar[9],selecionar[10])
-            return usuario
+        usuario = None
+        self.cursor.execute("SELECT * FROM usuario WHERE email = %s", (email,))
+        selecionar = self.cursor.fetchone()
+        if selecionar:
+            usuario = Usuario(*selecionar)
+        return usuario
     
     
-    
-
-
-    def cadastrarLivros(self, livro):
+    def cadastrarLivros(self, nome_autor, titulo, editora, isbn, edicao, volume, numero_pag, ano_publicacao, qntd_exemplar):
         """
         verifica se o codigo do livro informado corresponde aos dos livros ja cadastrados no sistema, caso nao, realiza o cadastro de um novo livro
 
         Parameters
         ----------
-        codigo_livro {string}:
-            codigo do livro em formato de string, que podera conter numero e/ou caracteres especiais
+        nome_autor {string}:
+            nome do autor do livro em formato de string.
+        titulo {string}:
+            titulo do livro em formato de string.
+        editora {string}:
+            nome da editora que publicou o livro em formato de string.
+        isbn {string}:
+            codigo de publicacao do livro em formato de string.
+        edicao {string}:
+            numero da edicao do livro em formato de string.
+        volume {int}:
+            numero do volume do livro em formato de numero inteiro.
+        numero_pag {int}:
+            quantidade total de paginas contidas no livro em formato de string.
+        ano_publicacao {string}:
+            ano em o livro foi publicado em formato de string.
+        qntd_exemplar {int}:
+            quantidade de exemplares que o livro possui em formato de numero inteiro.
         return {boolean}: 
             referente ao retorno com o resultado em formato Boleano (verdadeiro ou falso) das condicoes das funcoes 
         """
-        verifica = self.buscarLivros(livro.codigo_livro)
-        if verifica == None:
-            self.cursor.execute('INSERT INTO livro(codigo_livro, nome_autor, codigo_autor, titulo, editora, isbn, assunto, edicao, volume, Numero_pag, anoPublicacao, quantidade_exemplares) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %d)', (livro.codigo_livro, livro.nome_autor, livro.codigo_autor, livro.titulo, livro.editora, livro.isbn, livro.assunto, livro.edicao, livro.volume, livro.Numero_pag, livro.anoPublicacao, livro.quantidade_exemplares))
-            self.conexao.commit()
-            return True
-        else:
-            return False
+        for i in range(qntd_exemplar):
+            self.cursor.execute('INSERT INTO livro(nome_autor, titulo, editora, isbn, edicao, volume, numero_pag, ano_publicacao, ativo) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)', (nome_autor, titulo, editora, isbn, edicao, volume, numero_pag, ano_publicacao, 1))
+        self.conexao.commit()
 
-    def buscarLivros(self, codigo_livro):
+    def buscarLivros(self, pesquisa):
         """
-        realiza a busca de um livro pelo seu codigo.
+        realiza a busca de livros cadastrados no sistema pelo nome do autor/titulo do livro/nome da editora/codigo de publicação(isbn).
 
         Parameters
         ----------
-        codigo_livro {string}:
-            codigo do livro em formato de string, que podera conter numero e/ou caracteres especiais.
+        pesquisa:
+            pesquisa livros cadastrados no banco de dados do sistema pelo nome do autor/titulo do livro/nome da editora/codigo de publicação(isbn).
         return {boolean}: 
             referente ao retorno com o resultado do tipo None ou o proprio resultado das condicoes das funcoes
         livro:
             referece um elemento intanciado da classe Livro, que recebe uma lista. 
         """
-        self.cursor.execute("SELECT * FROM livro WHERE codigo_livro = %s", (codigo_livro,))
-        selecionar = self.cursor.fetchone()
-        if (selecionar == None):  # verifica se existe livro cadastrado
-            return None
-        else:
-            livro = Livro(selecionar[0],selecionar[1],selecionar[2],selecionar[3],selecionar[4],selecionar[5],selecionar[6],selecionar[7],selecionar[8],selecionar[9], selecionar[10], selecionar[11])
-            return livro
+        lista_livros = []
+        self.cursor.execute("SELECT * FROM livro WHERE nome_autor = %s OR titulo = %s OR editora = %s OR isbn = %s", (pesquisa, pesquisa, pesquisa, pesquisa))
+        selecionar = self.cursor.fetchall()
+        for registro in selecionar:
+            livro = Livro(*registro)
+            lista_livros.append(livro)
+        return lista_livros
 
+    def realizarEmprestimo(self, emprestimo): #precisa ser ajustada, quando realizar um emprestimo o exemplar deve ser removido
+        """
+        verifica se o codigo do exemplar informado corresponde aos dos exemplares com registro de emprestimo  no sistema, caso nao, realiza o emprestimo de um exemplar de livro
+
+        Parameters
+        ----------
+        codigo_exemplar {string}:
+            codigo do exemplar de um livro em formato de string, que podera conter numero e/ou caracteres especiais
+        return {boolean}: 
+            referente ao retorno com o resultado em formato Boleano (verdadeiro ou falso) das condicoes das funcoes 
+        """
+        verifica = self.buscarEmprestimo(emprestimo.codigo_exemplar)
+        if verifica == None:
+            self.cursor.execute('INSERT INTO emprestimo(codigo_livro, codigo_exemplar, data_emprestimo, data_para_devolver) VALUES(%s, %s, %s, %s)', (emprestimo.codigo_livro, emprestimo.codigo_exemplar, emprestimo.data_emprestimo, emprestimo.data_para_devolver))
+            #self.cursor.execute('DELETE FROM exemplar WHERE codigo_exemplar = %s', (exemplar.codigo_exemplar,))
+            self.conexao.commit()
+            return True
+        else:
+            return False
+    '''
     def cadastrarExemplares(self, exemplar):
         """
         verifica se o codigo do exemplar informado corresponde aos dos exemplares ja cadastrados no sistema, caso nao, realiza o cadastro de um novo exemplar
@@ -269,26 +282,7 @@ class Biblioteca:
             exemplar = Exemplar(selecionar[0],selecionar[1],selecionar[2])
             return exemplar
 
-    def realizarEmprestimo(self, emprestimo): #precisa ser ajustada, quando realizar um emprestimo o exemplar deve ser removido
-        """
-        verifica se o codigo do exemplar informado corresponde aos dos exemplares com registro de emprestimo  no sistema, caso nao, realiza o emprestimo de um exemplar de livro
-
-        Parameters
-        ----------
-        codigo_exemplar {string}:
-            codigo do exemplar de um livro em formato de string, que podera conter numero e/ou caracteres especiais
-        return {boolean}: 
-            referente ao retorno com o resultado em formato Boleano (verdadeiro ou falso) das condicoes das funcoes 
-        """
-        verifica = self.buscarEmprestimo(emprestimo.codigo_exemplar)
-        if verifica == None:
-            self.cursor.execute('INSERT INTO emprestimo(codigo_livro, codigo_exemplar, data_emprestimo, data_para_devolver) VALUES(%s, %s, %s, %s)', (emprestimo.codigo_livro, emprestimo.codigo_exemplar, emprestimo.data_emprestimo, emprestimo.data_para_devolver))
-            #self.cursor.execute('DELETE FROM exemplar WHERE codigo_exemplar = %s', (exemplar.codigo_exemplar,))
-            self.conexao.commit()
-            return True
-        else:
-            return False
-
+    
     def buscarEmprestimo(self, codigo_exemplar): 
         """
         realiza a busca de um emprestimo realizado pelo codigo do exemplar de livro.
@@ -354,4 +348,4 @@ class Biblioteca:
             devolucao = Devolucao(selecionar[0],selecionar[1],selecionar[2],selecionar[3],selecionar[4])
             return devolucao
         
-    
+    '''
